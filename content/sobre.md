df4b52dc4996044cb47eedaf780abda34398dceb
Yan Thomas <61414485+Yan-Thomas@users.noreply.github.com>
2022-10-20 14:56:30 -0300

    Add about page

---
+++ content/sobre.md
@@ -0,0 +1,7 @@
+---
+title: "Sobre o projeto"
+---
+
+# Sobre o projeto
+
+A CapyCode é uma plataforma educacional online interativa e 100% gratuita e open-source (de código livre).
---
+++ pages/sobre.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const { data: content } = await useAsyncData("about-content", () =>
+  queryContent().where({ title: "Sobre o projeto" }).findOne()
+);
+</script>
+
+<template>
+  <main>
+    <section>
+      <article>
+        <ContentRenderer class="flow-block" :value="content!">
+          <template #empty>
+            <p>Erro ao carregar conteúdo.</p>
+          </template>
+        </ContentRenderer>
+      </article>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding-top: var(--space-xl);
+  padding-inline: var(--space-s);
+}
+
+section {
+  max-width: 75ch;
+  height: 100%;
+  margin-inline: auto;
+}
+</style>
